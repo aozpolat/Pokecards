d1e3a35c13c629ca4d5f99c66c6750d57c2f0bbe
aozpolat <akdrozpolat@gmail.com>
2022-07-29 22:20:23 +0300

    Initial Commit

---
+++ Pokecards/PokecardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokecardsApp.swift
+//  Pokecards
+//
+//  Created by abdulk on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokecardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

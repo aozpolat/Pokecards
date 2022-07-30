77ce29861e293a242787d49c7eaac36cad7de707
aozpolat <akdrozpolat@gmail.com>
2022-07-30 15:28:31 +0300

    add restart button

--- Pokecards/ContentView.swift
+++ Pokecards/ContentView.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var pokemonVM = PokemonViewModel()
+//    @ObservedObject var pokemonVM = PokemonViewModel()
     var body: some View {
-        ScrollView {
-            ForEach(pokemonVM.pokemons) { item in
-                
-                VStack{
-                    HStack{
-                        Text(String(item.id))
-                        AsyncImage(url: item.imageUrl)
+        ZStack {
+            Color.indigo
+                .opacity(0.7)
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    Button {
+                    } label: {
+                        RestartButtonView()
                     }
-                    
+                    Spacer()
                 }
+                Spacer()
             }
-            
-           
+            .padding()
+            .padding(.top)
+
+            PokeCardView(pokemon: Pokemon(id: 1, name: "Bulbasaur", imageUrl: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")! , hp: 33, attack: 33, defense: 33))
+                .frame(width: 300, height: 400)
         }
     }
 }
@@ -30,5 +36,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RestartButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.white)
+                .frame(width: 50, height: 50)
+            Image(systemName: "arrow.clockwise")
+                .foregroundColor(.indigo.opacity(0.7))
+                .font(Font.system(size: 25, weight: .heavy))
+        }
     }
 }

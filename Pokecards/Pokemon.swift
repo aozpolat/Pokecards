fbcac0b4a2b3ad33a370fefb6f79c7f44361c3aa
aozpolat <akdrozpolat@gmail.com>
2022-07-30 11:37:56 +0300

    refactor pokemon models

--- Pokecards/Pokemon.swift
+++ Pokecards/Pokemon.swift
@@ -7,11 +7,42 @@
 
 import Foundation
 
-struct Pokemons: Codable {
-    var results: [Pokemon]
+//name and url for details
+struct PokemonBaseInfos: Decodable {
+    var results: [PokemonBaseInfo]
     
-    struct Pokemon: Codable{
+    struct PokemonBaseInfo: Decodable{
         var name: String
-        var url: String
+        var url: URL
     }
+}
+
+// Stat[0] -> hp, Stat[1] -> attack, Stat[2] -> defense
+struct PokemonDetail: Decodable {
+    var id: Int
+    var sprites: Sprite
+    var stats: [Stat]
+    
+    struct Sprite : Decodable{
+        var frontDefault: URL
+    }
+    
+    struct Stat : Decodable {
+        var baseStat:Int
+        var stat: StatDetail
+    }
+    
+    struct StatDetail : Decodable {
+        var name: String
+    }
+}
+
+//Pokemons to show inside UI
+struct Pokemon: Identifiable {
+    let id: Int
+    var name: String
+    var imageUrl: URL
+    var hp: Int
+    var attack: Int
+    var defense: Int
 }
